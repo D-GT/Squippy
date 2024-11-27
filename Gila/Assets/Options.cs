0e0e5cf7b17a256490e9aaf1beabe1fc9202a168
D-GT <19624184@mahasiswa.itb.ac.id>
2024-11-27 12:21:05 +0700

    update nig

--- Gila/Assets/Options.cs
+++ Gila/Assets/Options.cs
@@ -2,15 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 using TMPro;
 
 public class Options : MonoBehaviour
 {
     public TMP_Dropdown graphicsDropdown;
+    public Slider SliderMaster, SliderBGM, SliderSFX;
+    public AudioMixer Master;
 
     public void changeGraphicsQuality()
     {
         QualitySettings.SetQualityLevel(graphicsDropdown.value);
+    }
+    public void ChangeMasterVolume()
+    {
+        Master.SetFloat("MasterVol", SliderMaster.value);
+    }
+    public void ChangeBGMVolume()
+    {
+        Master.SetFloat("BGMVol", SliderBGM.value);
+    }
+    public void ChangeSFXVolume()
+    {
+        Master.SetFloat("SFXVol", SliderSFX.value);
     }
     // Start is called before the first frame update
     void Start()
